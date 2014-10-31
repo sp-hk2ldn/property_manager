ebf8d10f35f3b4da8b8ec1e6a2b9ac7863d2d6f1
Stephen <stephen.parker@timehascome.org>
2014-10-31 10:15:00 +0800

    giving up with modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require underscore
 //= require gmaps/google
-//= require bootstrap-modal
 //= require_tree .
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,6 +7,6 @@ class AdminController < ApplicationController
   	respond_to do |format|
   		format.html
   		format.js
+	end
   end
-end
 end
