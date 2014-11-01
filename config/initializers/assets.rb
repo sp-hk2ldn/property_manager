ab9fb21be87e190effa2e35ecc8438347febfeff
Stephen <stephen.parker@timehascome.org>
2014-11-01 22:40:01 +0800

    customising datatable

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( scripts.js )
 Rails.application.config.assets.precompile += %w( datatables.js )
+Rails.application.config.assets.precompile += %w( jquery.dataTables.css )
+Rails.application.config.assets.precompile += %w( jquery.dataTables.min.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
